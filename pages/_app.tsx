44d3e57f338d706cc6e5fb4939bc40b411aa5068
Joseeph5 <youssef.beenamor@gmail.com>
2022-09-04 21:22:31 +0100

    setup app structure

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,24 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import Script from 'next/script';
+import { ThemeProvider } from 'next-themes';
+
+import NavBar from '../components/NavBar';
+import Footer from '../components/Footer';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider attribute='class'>
+      <div className='dark:bg-nft-dark bg-white min-h-screen'>
+        <NavBar />
+        <Component {...pageProps} />
+        <Footer />
+      </div>
+      <Script
+        src='https://kit.fontawesome.com/9a0c2aa5e4.js'
+        crossOrigin='anonymous'></Script>
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
