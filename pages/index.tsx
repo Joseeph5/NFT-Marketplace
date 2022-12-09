e038f86f84a7edbde9ebefec01c04bf57166fda2
Joseeph5 <youssef.beenamor@gmail.com>
2022-12-09 15:22:05 +0100

    setup scroll cards

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,53 @@
-import { useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 
 import { Banner, CreatorCard } from '../components';
 import images from '../assets';
+import { useTheme } from 'next-themes';
 
 const Home: NextPage = () => {
-  const parentRef = useRef(null);
-  const scrollRef = useRef(null);
+  const parentRef = useRef<HTMLInputElement>(null);
+  const scrollRef = useRef<HTMLInputElement>(null);
+  const { theme } = useTheme();
+  const [hideButtons, setHideButtons] = useState<Boolean>(false);
+
+  const handleScroll = (direction: string) => {
+    const { current } = scrollRef;
+
+    const scrollAmount = window.innerWidth > 1800 ? 270 : 210;
+
+    if (direction === 'left') {
+      if (current) {
+        current.scrollLeft -= scrollAmount;
+      }
+    } else {
+      if (current) {
+        current.scrollLeft += scrollAmount;
+      }
+    }
+  };
+
+  // check if scrollRef container is overfilling its parentRef container
+  const isScrollable = () => {
+    const { current } = scrollRef;
+    const { current: parent } = parentRef;
+    if (current && parent) {
+      if (current?.scrollWidth >= parent?.offsetWidth) return setHideButtons(false);
+      return setHideButtons(true);
+    }
+  };
+
+  // if window is resized
+  useEffect(() => {
+    isScrollable();
+    window.addEventListener('resize', isScrollable);
+
+    return () => {
+      window.removeEventListener('resize', isScrollable);
+    };
+  });
 
   return (
     <div className="flex justify-center sm:px-4 p-12">
@@ -27,13 +66,40 @@ const Home: NextPage = () => {
           <h1 className="font-poppins dark:text-white text-nft-black-1 text-2xl minlg:text-4xl font-semibold ml-4 xs:ml-0">
             Best Creators
           </h1>
-          <div className="relative flex-1 max-w-full flex mt-3" ref={parentRef}></div>
-          <div
-            className="flex flex-row w-max overflow-x-scroll no-scrollbar select-none"
-            ref={scrollRef}>
-            {[1, 2, 3, 4, 5].map((creator, i) => (
-              <CreatorCard key={i} rank={i + 1} creatorImage={images[`creator`]} />
-            ))}
+          <div className="relative flex-1 max-w-full flex mt-3" ref={parentRef}>
+            <div
+              className="flex flex-row w-max overflow-x-scroll no-scrollbar select-none"
+              ref={scrollRef}>
+              {[1, 2, 3, 4, 5].map((creator, i) => (
+                <CreatorCard key={i} rank={i + 1} creatorImage={images[`creator`]} />
+              ))}
+              {!hideButtons && (
+                <div>
+                  <div
+                    onClick={() => handleScroll('left')}
+                    className="absolute w-8 h-8 minlg:w-12 minlg:h-12 top-45 cursor-pointer left-0">
+                    <Image
+                      src={images.left}
+                      layout="fill"
+                      objectFit="contain"
+                      alt="left_arrow"
+                      className={theme === 'light' ? 'filter invert' : undefined}
+                    />
+                  </div>
+                  <div
+                    onClick={() => handleScroll('right')}
+                    className="absolute w-8 h-8 minlg:w-12 minlg:h-12 top-45 cursor-pointer right-0">
+                    <Image
+                      src={images.right}
+                      layout="fill"
+                      objectFit="contain"
+                      alt="left_arrow"
+                      className={theme === 'light' ? 'filter invert' : undefined}
+                    />
+                  </div>
+                </div>
+              )}
+            </div>
           </div>
         </div>
       </div>
