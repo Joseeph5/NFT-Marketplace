aaa556c52ecfd2c14376ce421fb8ed1f9a905c58
Joseeph5 <youssef.beenamor@gmail.com>
2022-12-10 17:05:58 +0100

    create created-nfts page

--- pages/created-nfts.tsx
+++ pages/created-nfts.tsx
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const CreatedNfts = () => {
-  return <div>CreateNft</div>;
+  const [nfts, setNfts] = useState([1]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  if (!isLoading && nfts.length === 0) {
+    return (
+      <div className='flexCenter sm:p-4 p-16 min-h-screen'>
+        <h1 className='font-poppins dark:text-white text-nft-black-1 text-3xl font-extrabold'>
+          No NFTs Listed for Sale
+        </h1>
+      </div>
+    );
+  }
+  return (
+    <div className='flex justify-center sm:px-4 p-12 min-h-screen'>
+      <div className='w-full minmd:w-4/5'>
+        <div className='mt-4'>
+          <h2 className='font-poppins dark:text-white text-nft-black-1 text-2xl mt-2 ml-4 sm:ml-2 font-semibold'>
+            NFTs Listed for Sale
+          </h2>
+          <div className='mt-3 w-full flex flex-wrap justify-start md:justify-center'>
+            {nfts.map(() => {
+              return <div>NFT</div>;
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CreatedNfts;
