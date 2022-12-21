22edbc60a3329e6d881167ffcdccb09b9c0482f6
Joseeph5 <youssef.beenamor@gmail.com>
2022-12-21 20:58:28 +0100

    Update and Get Listing Price

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -12,9 +12,40 @@ contract NFTMarketplace is ERC721URIStorage {
     Counters.Counter private _tokenIds;
     Counters.Counter private _itemsSold;
 
+    uint256 listingPrice = 0.025 ether;
     address payable owner;
+
+    struct MarketItem {
+      uint256 tokenId;
+      address payable seller;
+      address payable owner;
+      uint256 price;
+      bool sold;
+    }
+
+    mapping(uint256 => MarketItem) private idToMarketItem;
+
+    event MarketItemCreated (
+      uint256 indexed tokenId,
+      address seller,
+      address owner,
+      uint256 price,
+      bool sold
+    );
+    
 
     constructor() ERC721("Metaverse Tokens", "METT") {
       owner = payable(msg.sender);
+    }
+
+    /* Updates the listing price of the contract */
+    function updateListingPrice(uint _listingPrice) public payable {
+      require(owner == msg.sender, "Only marketplace owner can update listing price.");
+      listingPrice = _listingPrice;
+    }
+
+    /* Returns the listing price of the contract */
+    function getListingPrice() public view returns (uint256) {
+      return listingPrice;
     }
 }
